23e05a466956ead9a9c22784386fd2586a4671ad
potkanburger <sehu@laposte.net>
2016-10-22 11:55:27 +0200

    init roll a ball

---
+++ Potkan_RollABall/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+	private int score;
+	public Text scoreText;
+	public Text winText;
+	private GameObject[] gos;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		score = 0;
+		SetCountText ();
+		winText.text = "";
+		gos = GameObject.FindGameObjectsWithTag("EndButton");
+		foreach (GameObject go in gos) {
+			go.SetActive (false);
+		}
+	}
+
+	void SetCountText(){
+		scoreText.text = "Score: " + score.ToString ();
+	}
+
+	void FixedUpdate(){
+
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		/*if (Input.accelerationEventCount > 0) {
+			moveHorizontal = Input.acceleration.x;
+			moveVertical = Input.acceleration.y;
+		}*/
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.CompareTag ("Pick Up")) {
+			other.gameObject.SetActive (false);
+			score++;
+			SetCountText ();
+			if (score >= 16) {
+				winText.text = "You win !";
+				foreach (GameObject go in gos) {
+					go.SetActive (true);
+				}
+			}
+		}
+	}
+}
---
+++ Potkan_RollABall/Assets/Scripts/ReplayScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ReplayScript : MonoBehaviour {
+
+    public void Replay()
+    {
+        SceneManager.LoadScene("main");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+}
